421db4f8ea060f412fa292624ba73fdadd83e3ba
Wasim Adetunji <wwa@live.ca>
2022-10-20 17:24:36 +0000

    Restored methods to class-level

--- app/javascript/packs/stackbiblio.jsx
+++ app/javascript/packs/stackbiblio.jsx
@@ -22,19 +22,6 @@ const clearTags = () => {
   this.setState({ selectedTags: [] })
 }
 
-const setSelectedTag = (userSelectedTag) => {
-  const selectedTags = this.state.selectedTags;
-  const existingTitles = selectedTags.map((entry) => { return entry.title; })
-
-  if (!existingTitles.includes(userSelectedTag.title)) {
-    selectedTags.push(userSelectedTag);
-    let tags = this.state.tags;
-    let unselected = tags.splice(tags.indexOf(userSelectedTag.title), 1);
-    this.setState({ selectedTags: selectedTags, tags: unselected });
-  }
-  this.render();
-}
-
 const renderSelection = (selectedItem, { handleClick, handleFocus, modifiers, query }) => {
   if (!modifiers.matchesPredicate) {
     return null;
@@ -53,19 +40,20 @@ const renderSelection = (selectedItem, { handleClick, handleFocus, modifiers, qu
     />
   );
 };
+const reduceTags = (accumulator, item) => {
+  item.tags.forEach((tag) => accumulator.add(tag));
+  return accumulator;
+}
 
 function App() {
   const db = new Dexie('stackbiblio-entries-development')
   db.version(1).stores({ entries: '++id, content, timestamp' })
   let allItems = useLiveQuery(() => db.entries.toArray(), []);
   if (!allItems) return null;
-
-  let set      = new Set();
+  let accumulator = new Set();
   // TODO: Let's add a separate tag store instead of this
-  allItems.forEach((item)=> { 
-    item.tags.forEach((tag) => { set.add(tag);}); 
-  })
-  let tagArray = Array.from(set);
+  let set         = allItems.reduce(reduceTags, accumulator)
+  let tagArray    = Array.from(set);
   
   return (
     <div>
@@ -74,7 +62,6 @@ function App() {
           clearTags={clearTags}
           entries={allItems}
           renderSelection={renderSelection}
-          setSelectedTag={setSelectedTag}
           tags={tagArray}
           tagsFilter={tagsFilter}
           />
@@ -84,7 +71,6 @@ function App() {
           db={db}
           entries={allItems}
           renderSelection={renderSelection}
-          setSelectedTag={setSelectedTag}
           tags={tagArray}
           tagsFilter={tagsFilter}
         />
