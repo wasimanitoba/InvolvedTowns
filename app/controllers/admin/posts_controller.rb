3139a0ad195820775c973d09c7b27fd86a255f13
Wasim Adetunji <wwa@live.ca>
2022-10-27 02:57:48 +0000

    Admin posts filtered by user

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,5 +1,5 @@
 class Admin::PostsController < ApplicationController
   def index
-    @posts = Thredded.posts_page_view(scope: Thredded::Post.all, current_user: current_user)
+    @posts = Thredded.posts_page_view(scope: Thredded::Post.where(user: params[:user_id]), current_user: current_user)
   end
 end
