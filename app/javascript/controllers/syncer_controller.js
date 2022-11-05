72c78c2a35f347d4334640b02a01d7e7c405f8cd
Wasim Adetunji <wwa@live.ca>
2022-11-05 21:34:38 +0000

    28 | Offline DB Management
    Adding save/delete/update buttons

--- app/javascript/controllers/syncer_controller.js
+++ app/javascript/controllers/syncer_controller.js
@@ -3,13 +3,27 @@ import PouchDB from 'pouchdb';
 
 // Connects to data-controller="syncer"
 export default class extends Controller {
+  static targets = ['password']
+
   connect() {
     this.db    = new PouchDB('stackbiblio-development');
-    this.tagDB = new PouchDB('tag-store-development');  
+    this.tagDB = new PouchDB('tag-store-development');
   }
 
   async sync() {
     // TODO: we don't want to hardcode the password or `localhost` here.
     this.db.sync('http://admin1:correctHorseBatteryStaple@localhost:5984/stackbiblio-development');
   }
+
+  createAccount() {
+    console.log(this.passwordTarget.value)
+  }
+
+  async save() {}
+
+  async delete() {}
+
+  async addTag() {}
+
+  async removeTag() {}
 }
