54a595eb8daadb17af993b2137e4cb373b393018
Wasim Adetunji <wwa@live.ca>
2022-11-09 15:19:18 +0000

    26 | Access Control
    - Added CanCanCan
    - Updated Ability.rb and notes_controller to restrict note access.
      Now limited to note owners and site admins

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,6 @@
 class NotesController < ApplicationController
   before_action :set_note, only: %i[ show edit update destroy ]
+  load_and_authorize_resource
 
   # GET /notes or /notes.json
   def index
