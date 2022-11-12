e63fc017d6770d91a3105ecca979d0776d163c1b
Wasim Adetunji <wwa@live.ca>
2022-11-12 05:50:01 +0000

    - #13 | Related Links
    This change introduces links between tags.
    
    - Each tag can be arbitrarily related to another tag.
    - This is done through a linked_tag join relation
    - We assign a 'predicate' to describe the relationship
    - One tag is the 'subject' of this predicate, and the other is the object
    
    This will allow us to construct a graph from these linkages.
    
    Added specs testing:
      - presence of subjects / objects
      - subjects_with_predicate() method with and without arguments
      - Nested routes
      - Comment and spec for bug ensuring destination is not origin of link
    
    Also added:
    
    - Nested routes
    - support for nested model in tag form handling

--- spec/views/tags/index.html.erb_spec.rb
+++ spec/views/tags/index.html.erb_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe 'tags/index', type: :view do
   before(:each) do
     user = User.create!(email: 'fake@fake.com', password: 'example', name: 'test user')
-    assign(:tags, [Tag.create!(title: 'Title', user: user), Tag.create!(title: 'Title', user: user)])
+    assign(:tags, [Tag.create!(title: "title_#{rand}", user: user), Tag.create!(title: "#{rand}_title", user: user)])
   end
 
   it 'renders a list of tags' do
