67f1df7165d642b6281ac9006a5dd8476500431c
Wasim Adetunji <wwa@live.ca>
2022-11-13 13:21:12 +0000

    Remove recogito

--- app/javascript/controllers/annotation_controller.js
+++ app/javascript/controllers/annotation_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
-import { Recogito } from '@recogito/recogito-js';
-import '@recogito/recogito-js/dist/recogito.min.css';
+
+
 export default class extends Controller {
   static targets = ['annotationPayload', 'form', 'tags']
 
@@ -8,15 +8,5 @@ export default class extends Controller {
     const self = this;
     const element = this.formTarget;
     self.annotationPayloadTarget.value = '';
-    self.recogito = new Recogito({ content: this.element });
-
-    self.recogito.on('createAnnotation', function(annotation) {
-      self.annotationPayloadTarget.value = JSON.stringify(annotation);
-      self.tagsTarget.value = annotation.body.filter((doc)=> { return doc.purpose === 'tagging'} ).map((doc)=> {return doc.value; }).join(',')
-      element.requestSubmit();
-      // element.submit()
-      // element.dispatchEvent(new Event('submit', {bubbles: false}));
-      // Rails.fire(element, 'submit');
-    });
   }
 }
