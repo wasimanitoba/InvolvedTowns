4e01314db51ca13004cb9e9f50a457fb0dbbe0c6
Wasim Adetunji <wwa@live.ca>
2022-11-14 14:51:57 +0000

    Rename split stack/library

--- app/controllers/stack_controller.rb
+++ app/controllers/stack_controller.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-class StackbiblioController < ApplicationController
+class StackController < ApplicationController
   def messaging
     @hide_chat = true
     @page_name = 'chat-body-class'
   end
-
-  def cloud_library; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,16 +18,17 @@ Rails.application.routes.draw do
     resources :tags, :collections, :bookmarks, :reminders, :notes, :posts
   end
 
-  scope('/library') do
-    get '/import', to: 'stackbiblio#import', as: :notes_import
+  get '/library', to: 'library#index', as: :library
+
+  scope('/stack') do
+    get '/import', to: 'stack#import', as: :notes_import
     post '/import', to: 'bookmarks#bulk_upload'
-    get '/cloud', to: 'stackbiblio#cloud_library', as: :cloud_library
-    get '/list', to: 'stackbiblio#list', as: :offline_note_list
-    get '/sync', to: 'stackbiblio#sync', as: :sync_library
-    get '/', to: 'stackbiblio#index'
+    get '/list', to: 'stack#list', as: :offline_note_list
+    get '/sync', to: 'stack#sync', as: :sync_library
+    get '/', to: 'stack#index'
   end
 
-  get '/messaging', to: 'stackbiblio#messaging', as: :messages
+  get '/messaging', to: 'stack#messaging', as: :messages
   get '/me', to: 'users#me', as: :me
   get '/about', to: 'home#index'
   root to: redirect('/home')
