0efd5b11744d684c6c1cdf7ef10b3ad51c9f5604
Wasim Adetunji <wwa@live.ca>
2022-11-14 14:29:32 +0000

    Fix controller name

--- app/controllers/admin/notes_controller.rb
+++ app/controllers/admin/notes_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class NotesController < ApplicationController
+class Admin::NotesController < ApplicationController
   before_action :set_note, only: %i[show edit update destroy]
   load_and_authorize_resource
 
