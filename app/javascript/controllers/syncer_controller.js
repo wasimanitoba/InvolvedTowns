b8bbd0fbcebe1f146b65356ab6b659e7609f5f58
Wasim Adetunji <wwa@live.ca>
2022-11-24 01:41:01 +0000

    Added method for identifying user DB

--- app/javascript/controllers/syncer_controller.js
+++ app/javascript/controllers/syncer_controller.js
@@ -14,15 +14,22 @@ export default class extends Controller {
   async sync() {
     let password = prompt('Provide your password to access your account:')
     if (password === null) { alert('Sorry, cannot access your account without a password.'); return; }
-    // TODO: we don't want to hardcode the password or `localhost` here.
-    this.db.sync(`http://${this.emailValue}:${password}@localhost:5984/stackbiblio-development`);
+
+    this.db.sync(`http://${this.emailValue}:${password}@localhost:5984/${this.getUserDatabaseName(this.emailValue)}`);
   }
 
-  async save() {}
-
-  async delete() {}
-
-  async addTag() {}
-
-  async removeTag() {}
+/** from: https://terreii.github.io/use-pouchdb/docs/basics/sync
+ * Get the name of the users remote-database.
+ * This function uses browser APIs.
+ * @param {string} name     - The username.
+ * @param {string} [prefix] - Prefix, can be changed with config [couch_peruser] database_prefix
+ */
+ getUserDatabaseName(name, prefix = 'userdb-') {
+  const encoder = new TextEncoder()
+  const buffy = encoder.encode(name)
+  const bytes = Array.from(buffy).map(byte =>
+    byte.toString(16).padStart(2, '0')
+  )
+  return prefix + bytes.join('')
+}
 }
